1f47a00deb55f3203fd2279cc7ac2d9dfddee165
jean luc kawel <20ka208@esisalama.org>
2024-01-17 11:25:51 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------->
 | Web Routes
 |--------------------------------------------------------------------------
 |
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/index',[App\Http\Controllers\BlogController::class,'index'])->name('index');
+
+Route::get('/show/{id}',[App\Http\Controllers\BlogController::class,'show'])->name('blog.show');
+
+
+
