2884b1dcb5124e095f2f89eb00a07be3d79b1ead
jean luc kawel <20ka208@esisalama.org>
2024-01-17 13:18:07 +0200

    add laravel file

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-Route::get('/index',[App\Http\Controllers\BlogController::class,'index'])->name('index');
-
-Route::get('/show/{id}',[App\Http\Controllers\BlogController::class,'show'])->name('blog.show');
-
-
-
